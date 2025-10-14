3794da6a814544f94ca752e70bb2dd81a612ef70
Kimblag <kimberly.blandong@gmail.com>
2025-10-14 17:45:12 -0300

    Se crea template con estructura inicial para el menú lateral.

---
+++ CatalogoApp/CatalogoApp.UI/Resources/Styles/Templates.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Input;
+
+namespace CatalogoApp.UI.Resources.Styles;
+
+public partial class Templates : ResourceDictionary
+{
+    
+    public Templates()
+	{
+		InitializeComponent();
+    }
+
+   
+
+}

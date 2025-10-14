2a5b167867ade4dea9e71babdf6783535aa40cce
Kimblag <kimberly.blandong@gmail.com>
2025-10-14 15:48:28 -0300

    Se definen los métodos para el repositorio de Marcas

--- CatalogoApp/CatalogoApp.Data/Repositorios/RepositorioMarcaSQL.cs
+++ CatalogoApp/CatalogoApp.Data/Repositorios/RepositorioMarcaSQL.cs
@@ -1,5 +1,6 @@
 ﻿using CatalogoApp.Core.Entidades;
 using CatalogoApp.Core.Interfaces;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
 namespace CatalogoApp.Data.Repositorios
@@ -19,22 +20,86 @@ namespace CatalogoApp.Data.Repositorios
 
         public void Actualizar(Marca marca)
         {
-            throw new NotImplementedException();
+            const string query = @"UPDATE Marcas SET Descripcion = @Descripcion WHERE IdMarca = @IdMarca";
+
+            using (SqlConnection conexion = new SqlConnection(_stringConnection))
+            {
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.Text;
+                    comando.Parameters.AddWithValue("@IdMarca", marca.IdMarca);
+                    comando.Parameters.AddWithValue("@Descripcion", marca.Descripcion);
+                    comando.Connection.Open();
+                    comando.ExecuteNonQuery();
+                }
+            }
         }
 
         public void Agregar(Marca nuevaMarca)
         {
-            throw new NotImplementedException();
+            const string query = @"INSERT INTO Marcas (Descripcion) VALUES (@Descripcion)";
+
+            using (SqlConnection conexion = new SqlConnection(_stringConnection))
+            {
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.Text;
+                    comando.Parameters.AddWithValue("@Descripcion", nuevaMarca.Descripcion);
+                    comando.Connection.Open();
+                    comando.ExecuteNonQuery();
+                }
+            }
         }
 
         public void Eliminar(int idMarca)
         {
-            throw new NotImplementedException();
+            const string query = @"DELETE FROM Marcas WHERE IdMarca = @IdMarca";
+
+            using (SqlConnection conexion = new SqlConnection(_stringConnection))
+            {
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.Text;
+                    comando.Parameters.AddWithValue("@IdMarca", idMarca);
+                    comando.Connection.Open();
+                    comando.ExecuteNonQuery();
+                }
+            }
         }
 
         public IEnumerable<Marca> Listar()
         {
-            throw new NotImplementedException();
+            const string query = @"SELECT MA.IdMarca, MA.Descripcion FROM Marcas MA";
+            List<Marca> marcas = new List<Marca>();
+
+            using (SqlConnection conexion = new SqlConnection(_stringConnection))
+            {
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.Text;
+                    comando.Connection.Open();
+
+                    using (SqlDataReader lector = comando.ExecuteReader())
+                    {
+                        if (lector.HasRows)
+                        {
+                            int idOrdinal = lector.GetOrdinal("IdMarca");
+                            int descOrdinal = lector.GetOrdinal("Descripcion");
+
+                            while (lector.Read())
+                            {
+                                Marca marcaActual = new Marca
+                                {
+                                    IdMarca = lector.GetInt32(idOrdinal),
+                                    Descripcion = lector.GetString(descOrdinal)
+                                };
+                                marcas.Add(marcaActual);
+                            }
+                        }
+                    }
+                }
+            }
+            return marcas;
         }
     }
 }

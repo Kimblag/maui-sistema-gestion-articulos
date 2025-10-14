c8823e1af1885c9f0990f41e4cad886075f63128
Kimblag <kimberly.blandong@gmail.com>
2025-10-14 15:53:39 -0300

    Se definen las rutas de navegación del proyecto y se agrega la lógica necesaria para redirigir a cada página.

--- CatalogoApp/CatalogoApp.UI/AppShell.xaml.cs
+++ CatalogoApp/CatalogoApp.UI/AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace CatalogoApp.UI
+﻿using CatalogoApp.UI.ViewModels;
+
+namespace CatalogoApp.UI
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(ArticuloPage), typeof(ArticuloPage));
+            Routing.RegisterRoute(nameof(MarcaPage), typeof(MarcaPage));
+            Routing.RegisterRoute(nameof(CategoriaPage), typeof(CategoriaPage));
         }
     }
 }

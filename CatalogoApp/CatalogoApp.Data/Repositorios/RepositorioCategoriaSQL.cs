6ec2cd58e272e1cce0206e4a6865e2be8e767dc1
Kimblag <kimberly.blandong@gmail.com>
2025-10-14 15:47:48 -0300

    Se definen los métodos para el repositorio de Categorías

--- CatalogoApp/CatalogoApp.Data/Repositorios/RepositorioCategoriaSQL.cs
+++ CatalogoApp/CatalogoApp.Data/Repositorios/RepositorioCategoriaSQL.cs
@@ -1,6 +1,8 @@
 ﻿using CatalogoApp.Core.Entidades;
 using CatalogoApp.Core.Interfaces;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System.Text.RegularExpressions;
 
 namespace CatalogoApp.Data.Repositorios
 {
@@ -19,22 +21,86 @@ namespace CatalogoApp.Data.Repositorios
 
         public void Actualizar(Categoria categoria)
         {
-            throw new NotImplementedException();
+            const string query = @"UPDATE Categorias SET Descripcion = @Descripcion WHERE IdCategoria = @IdCategoria";
+
+            using (SqlConnection conexion = new SqlConnection(_stringConnection))
+            {
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.Text;
+                    comando.Parameters.AddWithValue("@IdCategoria", categoria.IdCategoria);
+                    comando.Parameters.AddWithValue("@Descripcion", categoria.Descripcion);
+                    comando.Connection.Open();
+                    comando.ExecuteNonQuery();
+                }
+            }
         }
 
         public void Agregar(Categoria nuevaCategoria)
         {
-            throw new NotImplementedException();
+            const string query = @"INSERT INTO Categorias (Descripcion) VALUES (@Descripcion)";
+
+            using (SqlConnection conexion = new SqlConnection(_stringConnection))
+            {
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.Text;
+                    comando.Parameters.AddWithValue("@Descripcion", nuevaCategoria.Descripcion);
+                    comando.Connection.Open();
+                    comando.ExecuteNonQuery();
+                }
+            }
         }
 
         public void Eliminar(int idCategoria)
         {
-            throw new NotImplementedException();
+            const string query = @"DELETE FROM Categorias WHERE IdCategoria = @IdCategoria";
+
+            using (SqlConnection conexion = new SqlConnection(_stringConnection))
+            {
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.Text;
+                    comando.Parameters.AddWithValue("@IdCategoria", idCategoria);
+                    comando.Connection.Open();
+                    comando.ExecuteNonQuery();
+                }
+            }
         }
 
         public IEnumerable<Categoria> Listar()
         {
-            throw new NotImplementedException();
+            const string query = @"SELECT CAT.IdCategoria, CAT.Descripcion FROM Categorias CAT";
+            List<Categoria> categorias = new List<Categoria>();
+
+            using (SqlConnection conexion = new SqlConnection(_stringConnection))
+            {
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.Text;
+                    comando.Connection.Open();
+
+                    using (SqlDataReader lector = comando.ExecuteReader())
+                    {
+                        if (lector.HasRows)
+                        {
+                            int idOrdinal = lector.GetOrdinal("IdCategoria");
+                            int descOrdinal = lector.GetOrdinal("Descripcion");
+
+                            while (lector.Read())
+                            {
+                                Categoria categoriaActual = new Categoria
+                                {
+                                    IdCategoria = lector.GetInt32(idOrdinal),
+                                    Descripcion = lector.GetString(descOrdinal)
+                                };
+                                categorias.Add(categoriaActual);
+                            }
+                        }
+                    }
+                }
+            }
+            return categorias;
         }
     }
 }
